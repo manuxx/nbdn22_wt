948754c87968f7dfb2849b39a13ee9bae303e665
manuxx <manusxyz@gmail.com>
2022-05-17 13:26:15 +0200

    rozwiazanioe

--- MovieLibrary/MovieLibrary.cs
+++ MovieLibrary/MovieLibrary.cs
@@ -28,5 +28,23 @@ namespace TrainingPrep.collections
             }
             movies.Add(movie);
         }
+
+        public IEnumerable<Movie> all_movies_published_by_pixar()
+        {
+            foreach (var movie in movies)
+            {
+                if (movie.production_studio == ProductionStudio.Pixar)
+                {
+                    yield return movie;
+                }
+            }
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_title_descending()
+        {
+            var ret = new List<Movie>(movies);
+            ret.Sort((m1,m2)=> (-1)* m1.title.CompareTo(m2.title));
+            return ret;
+        }
     }
 }
